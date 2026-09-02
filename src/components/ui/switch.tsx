5e2fef0ad091d8deb49e2e04549c054d9905ecad
abhikatta <abhinaykatta97@gmail.com>
2026-09-02 23:53:05 +0530

    feat: compute x translate positions using css variables for default and sm

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -3,39 +3,37 @@ import { cva } from "class-variance-authority";
 import { useState } from "react";
 export type SIZE_VARIANTS = "sm" | "md" | "lg" | "default";
 
-const switchContainerVariants = cva(
-  "flex items-center rounded-2xl transition-colors",
-  {
-    variants: {
-      variant: {
-        sm: "h-4.5 w-10 px-0.25",
-        md: "h-9 w-20",
-        lg: "h-12 w-32",
-        default: "h-7 w-16 px-0.5",
-      },
-      checked: {
-        true: "bg-green-400",
-        false: "bg-gray-200",
-      },
+const switchVariants = cva("flex items-center rounded-2xl transition-colors", {
+  variants: {
+    variant: {
+      sm: "h-4.5 w-10 px-0.25",
+      md: "h-9 w-20",
+      lg: "h-12 w-32",
+      default: "h-7 w-16 px-0.5",
     },
-    defaultVariants: {
-      variant: "default",
+    checked: {
+      true: "bg-green-400",
+      false: "bg-gray-200",
     },
   },
-);
+  defaultVariants: {
+    variant: "default",
+  },
+});
 
 const switchThumbVariants = cva(
   "rounded-full bg-white transition-all ease-out focus-visible:border-none focus-visible:outline-1",
   {
     variants: {
       variant: {
-        sm: "h-4 w-6",
+        sm: "h-4 w-6 [--padding:0.0625rem] [--switch-width:2.5rem] [--thumb-width:1.5rem]",
         md: "",
         lg: "",
-        default: "h-6 w-9",
+        default:
+          "h-6 w-9 [--padding:0.25rem] [--switch-width:4rem] [--thumb-width:2rem]",
       },
       checked: {
-        true: "translate-x-[calc(4rem-2.25rem-2*0.125rem)]",
+        true: "translate-x-[calc(var(--switch-width)-var(--thumb-width)-2*var(--padding))]",
         false: "translate-x-0",
       },
       held: {
@@ -48,16 +46,15 @@ const switchThumbVariants = cva(
     },
     compoundVariants: [
       {
-        variant: "default",
         held: true,
         checked: true,
-        className: "translate-x-[calc(4rem-2rem-4*0.125rem)]",
+        className:
+          "translate-x-[calc(var(--switch-width)-var(--thumb-width)-4*var(--padding))]",
       },
       {
-        variant: "default",
         held: true,
         checked: false,
-        className: "translate-x-0.125",
+        className: "translate-x-[var(--padding)]",
       },
     ],
   },
@@ -99,9 +96,7 @@ export const Switch = ({
       onMouseDown={() => setIsHeldDown(true)}
       onMouseUp={resetHeld}
       onClick={handleCheckedChange}
-      className={cn(
-        switchContainerVariants({ variant, checked: derivedChecked }),
-      )}
+      className={cn(switchVariants({ variant, checked: derivedChecked }))}
     >
       <button
         id="thumb"
